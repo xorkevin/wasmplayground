875e18b943bfee364311f1dc28e8d60f827d18fc
Kevin Wang <kevin@xorkevin.com>
2023-09-21 01:51:51 -0700

    Create WasmModInstance abstraction

--- wasmmod.js
+++ wasmmod.js
@@ -6,76 +6,90 @@ const textEncoder = new TextEncoder();
 // TextDecoder only supports utf-8
 const textDecoder = new TextDecoder();
 
+class WasmModInstance {
+  constructor(mod) {
+    this.mod = mod;
+  }
+
+  async init() {
+    this.instance = await WebAssembly.instantiate(this.mod, {
+      __wasm_import: {
+        __js_throw: (ptr, size) => {
+          const mem = new Uint8Array(
+            this.instance.exports.memory.buffer,
+            ptr,
+            size,
+          );
+          throw new Error(`error from wasm mod: ${textDecoder.decode(mem)}`);
+        },
+      },
+    });
+
+    this.HEADER_SIZE = this.instance.exports.get_malloc_header_size();
+    if (this.HEADER_SIZE !== 4) {
+      throw new Error('header size greater than 4 bytes');
+    }
+  }
+
+  mallocBytes(b) {
+    const ptr = this.instance.exports.malloc(b.length);
+    const mem = new Uint8Array(
+      this.instance.exports.memory.buffer,
+      ptr + this.HEADER_SIZE,
+      b.length,
+    );
+    mem.set(b);
+    return ptr;
+  }
+
+  free(ptr) {
+    this.instance.exports.free(ptr);
+  }
+
+  readBytes(ptr) {
+    const size = new DataView(
+      this.instance.exports.memory.buffer,
+      ptr,
+      this.HEADER_SIZE,
+    ).getUint32(0, false);
+    const mem = new Uint8Array(
+      this.instance.exports.memory.buffer,
+      ptr + this.HEADER_SIZE,
+      size,
+    );
+    return mem;
+  }
+
+  mallocString(s) {
+    const b = textEncoder.encode(s);
+    return this.mallocBytes(b);
+  }
+
+  readStringAndFree(ptr) {
+    try {
+      const mem = this.readBytes(ptr);
+      const str = textDecoder.decode(mem);
+      return str;
+    } finally {
+      this.free(ptr);
+    }
+  }
+
+  callStrFn(name, arg) {
+    const argptr = this.mallocString(arg);
+    try {
+      const res = this.instance.exports[name](argptr);
+      return this.readStringAndFree(res);
+    } finally {
+      this.free(argptr);
+    }
+  }
+}
+
 const wasmbuf = await fs.readFile('./bin/wasmplayground.wasm');
 const wasmmod = await WebAssembly.compile(wasmbuf);
 
-let wasmInstance;
-wasmInstance = await WebAssembly.instantiate(wasmmod, {
-  __wasm_import: {
-    __js_throw: (ptr, size) => {
-      const mem = new Uint8Array(wasmInstance.exports.memory.buffer, ptr, size);
-      throw new Error(`error from wasm mod: ${textDecoder.decode(mem)}`);
-    },
-  },
-});
+const wasmModInstance = new WasmModInstance(wasmmod);
+await wasmModInstance.init();
 
-const HEADER_SIZE = wasmInstance.exports.get_malloc_header_size();
-if (HEADER_SIZE != 4) {
-  throw new Error('header size not 4 bytes');
-}
-
-const mallocBytes = (b) => {
-  const ptr = wasmInstance.exports.malloc(b.length);
-  const mem = new Uint8Array(
-    wasmInstance.exports.memory.buffer,
-    ptr + HEADER_SIZE,
-    b.length,
-  );
-  mem.set(b);
-  return ptr;
-};
-
-const free = (ptr) => {
-  wasmInstance.exports.free(ptr);
-};
-
-const readBytes = (ptr) => {
-  const size = new DataView(
-    wasmInstance.exports.memory.buffer,
-    ptr,
-    HEADER_SIZE,
-  ).getUint32(0, false);
-  const mem = new Uint8Array(
-    wasmInstance.exports.memory.buffer,
-    ptr + HEADER_SIZE,
-    size,
-  );
-  return mem;
-};
-
-const mallocString = (s) => {
-  const b = textEncoder.encode(s);
-  return mallocBytes(b);
-};
-
-const readStringAndFree = (ptr) => {
-  try {
-    const mem = readBytes(ptr);
-    const str = textDecoder.decode(mem);
-    return str;
-  } finally {
-    free(ptr);
-  }
-};
-
-const greet = (name) => {
-  const namearg = mallocString(name);
-  try {
-    const greeting = wasmInstance.exports.greet(namearg);
-    return readStringAndFree(greeting);
-  } finally {
-    free(namearg);
-  }
-};
-
-console.log(greet('world'));
+console.log(wasmModInstance.callStrFn('greet', 'world'));
